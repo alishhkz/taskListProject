1869a2e85684baf7717354f36912e7d4ffd07286
alishhkz <alisherrmukanov04@gmail.com>
2024-05-28 15:27:46 +0500

    initial commit

---
+++ taskList/src/main/java/com/example/taskList/web/security/JwtUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.taskList.web.security;
+
+import com.example.taskList.domain.user.User;
+import com.example.taskList.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getByUsername(username);
+        return JwtEntityFactory.create(user);
+    }
+}
